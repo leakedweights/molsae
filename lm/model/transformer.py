9347507bbaf9ae8275c937277120ca37593653d4
leakedweights <vkristof20@protonmail.com>
2024-10-26 05:15:12 +0200

    fixed missing attr

--- lm/model/transformer.py
+++ lm/model/transformer.py
@@ -21,9 +21,9 @@ class TransformerBlock(nn.Module):
     @nn.compact
     def __call__(self, x, pos, mask):
 
-        if self.saes is not None:
+        if self.modifiers is not None:
             assert len(
-                self.saes) == 2, "SAEs must be defined for post-norm MLP and residual stream."
+                self.modifiers) == 2, "Modifiers must be defined for post-norm MLP and residual stream."
 
         y = nn.RMSNorm()(x)
         y = GroupedQueryAttention(self.d_model,
@@ -41,15 +41,22 @@ class TransformerBlock(nn.Module):
         y = FeedForward(self.d_model, self.hidden_dim)(y)
         mlp_post_norm = nn.RMSNorm()(y)
 
+        mlp_mod_act = ()
         if self.modifiers is not None and self.modifiers[0] is not None:
-            mlp_post_norm = self.modifiers[0](mlp_post_norm)
+            mlp_post_norm, mlp_mod_act = self.modifiers[0](mlp_post_norm)
 
         residual = x + mlp_post_norm
 
+        residual_mod_act = ()
         if self.modifiers is not None and self.modifiers[1] is not None:
-            residual = self.modifiers[1](residual)
+            residual, residual_mod_act = self.modifiers[1](residual)
 
-        return residual, ((mlp_post_norm, residual) if self.tracked else ())
+        if self.tracked:
+            extras = (mlp_post_norm, residual), (mlp_mod_act, residual_mod_act)
+        else:
+            extras = (tuple(), tuple())
+
+        return residual, extras
 
 
 class Decoder(nn.Module):
@@ -81,15 +88,15 @@ class Decoder(nn.Module):
             modifiers = [(None, None)] * self.num_layers
 
         for block_id in range(self.num_layers):
-            x, actv, modifier_actv = TransformerBlock(self.d_model,
-                                                      self.hidden_dim,
-                                                      self.head_dim,
-                                                      self.num_heads,
-                                                      self.num_kv_heads,
-                                                      self.layer_logit_cap,
-                                                      self.f_embed,
-                                                      self.tracked,
-                                                      modifiers[block_id])(x, pos, mask)
+            x, (actv, modifier_actv) = TransformerBlock(self.d_model,
+                                                        self.hidden_dim,
+                                                        self.head_dim,
+                                                        self.num_heads,
+                                                        self.num_kv_heads,
+                                                        self.layer_logit_cap,
+                                                        self.f_embed,
+                                                        self.tracked,
+                                                        modifiers[block_id])(x, pos, mask)
 
             if self.tracked and (not aux_ids or block_id in aux_ids):
                 activations.append(actv)
@@ -105,5 +112,7 @@ class Decoder(nn.Module):
         logits = self.final_logit_cap * jnp.tanh(logits / self.final_logit_cap)
 
         if self.tracked:
-            return logits, activations, modifier_activations
+            if self.modifiers is not None:
+                return logits, activations, modifier_activations
+            return logits, activations
         return logits
